1073400559004152219e7ccf1797d149ea5ac4cf
leirisson <103783610+leirisson@users.noreply.github.com>
2026-01-12 18:58:08 -0400

    feat: add environment configuration with dotenv and zod, update database connection settings

---
+++ 4_fastify_com_knex/src/env/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
+  DIRECTORY: z.string(),
+})
+
+export const env = envSchema.parse(process.env)
--- 4_fastify_com_knex/src/server.ts
+++ 4_fastify_com_knex/src/server.ts
@@ -1,16 +1,31 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { app } from './app'
 import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
-  const tables = await knex('sqlite_schema').select('*')
+  const tables = await knex('transactions').select('*')
   return reply.send(tables)
+})
+
+app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
+  const { title, amount } = request.body as any
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title,
+      amount,
+    })
+    .returning('*')
+
+  return reply.send(transaction)
 })
 
 async function startServer() {
   try {
     app.listen({
-      port: 3333,
+      port: env.PORT,
     })
     console.log(`HTTP SERVER FUNCIONANDO EM: http://localhost:3333`)
   } catch (error) {

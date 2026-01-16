d616c81589d619f8ccac2db8519e8191e65ac44d
leirisson <103783610+leirisson@users.noreply.github.com>
2026-01-15 21:44:53 -0400

    feat: add session management with cookies and error handling in transactions routes

--- 7_api_transactions/src/app.ts
+++ 7_api_transactions/src/app.ts
@@ -1,6 +1,34 @@
 import fastify from 'fastify'
 import { routes } from './routes'
+import fastifyCookie from '@fastify/cookie'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
+app.register(fastifyCookie)
+
+
+// 🔹 trtando os Errors Handler GLOBAL
+// posso tratar os erros de dominio por aqui
+app.setErrorHandler((error, request, reply) => {
+
+  if (error) {
+    return reply.status(401).send({
+      error: 'Unauthorized'
+    })
+  }
+
+  // Zod (se estiver usando)
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      error: 'Validation error',
+      issues: error.format(),
+    })
+  }
+
+  // Erro genérico
+  return reply.status(500).send({
+    error: 'Internal Server Error'
+  })
+})
 app.register(routes)
--- 7_api_transactions/src/routes/route-transactions.ts
+++ 7_api_transactions/src/routes/route-transactions.ts
@@ -2,31 +2,43 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { knex } from '../config-database'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
+import { checkSessionExists } from '../middlewares/check-session-exists'
 
 
 
 export async function transactionsRoutes(app: FastifyInstance) {
-    app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
-        const transactions = await knex('transactions').select('*')
-        return reply.send({ transactions })
-    })
+    app.get('/', { preHandler: [checkSessionExists] },
+        async (request: FastifyRequest, reply: FastifyReply) => {
 
-    app.get('/:id', async (request: FastifyRequest, reply: FastifyReply) => {
+            const { sessionId } = request.cookies
+
+            console.log(sessionId)
+
+            const transactions = await knex('transactions')
+                .where('session_id', sessionId)
+                .select('*')
+            return reply.send({ transactions })
+        })
+
+    app.get('/:id', { preHandler: [checkSessionExists] }, async (request: FastifyRequest, reply: FastifyReply) => {
         const getTransactionParamsSchema = z.object({
             id: z.uuid()
         })
 
         const { id } = getTransactionParamsSchema.parse(request.params)
-        
-        const transaction = await knex('transactions').where({id,}).first()
 
-        return reply.send({transaction})
+        const transaction = await knex('transactions').where({ id, }).first()
+
+        return reply.send({ transaction })
     })
 
-    app.get('/sumary', async () => {
-        const sumary = await knex('transactions').sum('amount', {as:'amount'}).first()
+    app.get('/sumary', { preHandler: [checkSessionExists] }, async (request: FastifyRequest) => {
+        const { sessionId } = request.cookies
+        const sumary = await knex('transactions')
+            .where('session_id', sessionId)
+            .sum('amount', { as: 'amount' }).first()
 
-        return {sumary}
+        return { sumary }
     })
 
     app.post('/', async (request: FastifyRequest, reply: FastifyReply) => {
@@ -39,10 +51,24 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
         const { amount, title, type } = createTransactionBodySchema.parse(request.body)
 
+
+        // criando o cookies
+        let sessionId = request.cookies.sessionId
+
+        if (!sessionId) {
+            sessionId = randomUUID()
+
+            reply.cookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 7 // vai durar 7 dias
+            })
+        }
+
         await knex('transactions').insert({
             id: randomUUID(),
             title,
-            amount: type === 'credit' ? amount : amount * -1
+            amount: type === 'credit' ? amount : amount * -1,
+            session_id: sessionId
         })
 
         return reply.status(201).send()

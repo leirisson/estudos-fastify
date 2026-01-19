803b75f080a0475984fcd2f8feec8875b368b8bc
leirisson <103783610+leirisson@users.noreply.github.com>
2026-01-19 17:12:24 -0400

    feat: add meal creation route with validation and database integration

--- 9_Daily_Diet_API/src/routes/index.ts
+++ 9_Daily_Diet_API/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { apiRouteTest } from './test-routes'
 import { userRoutes } from './user-routes'
+import { mealRoutes } from './meal-routes'
 
 export function routes(app: FastifyInstance) {
 
     app.register(apiRouteTest, { prefix: "/test" })
-    app.register(userRoutes, {prefix: '/users'})
+    app.register(userRoutes, { prefix: '/users' })
+    app.register(mealRoutes, { prefix: '/meal' })
 }
--- 9_Daily_Diet_API/src/routes/meal-routes.ts
+++ 9_Daily_Diet_API/src/routes/meal-routes.ts
@@ -1,1 +1,35 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
+import { knex } from '../config-database'
+import z, { date } from 'zod'
+import { randomUUID } from 'node:crypto'
+
+export async function mealRoutes(app: FastifyInstance) {
+    app.post('/create', async (request: FastifyRequest, reply: FastifyReply) => {
+        const createBodySchemaMeal = z.object({
+            name: z.string(),
+            user_id: z.string(),
+            description: z.string(),
+            data: z.string(),
+            time: z.string(),
+            is_diet: z.boolean().default(true)
+        })
+
+        const { name, user_id,  description, time, data, is_diet } = createBodySchemaMeal.parse(request.body)
+
+        const data_completed = new Date(`${data} ${time}`) 
+
+        await knex('meal').insert({
+            id: randomUUID(),
+            name,
+            user_id,
+            description,
+            is_diet,
+            created_at: data_completed
+        })
+
+        return reply.status(201).send()
+
+    })
+
+
+}

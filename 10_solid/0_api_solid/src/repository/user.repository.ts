babf473e237ebeb8cd14b8dcc3c516bf3372170f
leirisson <103783610+leirisson@users.noreply.github.com>
2026-01-22 00:38:27 -0400

    refactor: remove unnecessary newline in UserRepository interface

--- 10_solid/0_api_solid/src/repository/user.repository.ts
+++ 10_solid/0_api_solid/src/repository/user.repository.ts
@@ -2,6 +2,5 @@ import { User, Prisma } from 'generated/prisma'
 
 export interface UserRepository {
     create(data: Prisma.UserCreateInput): Promise<User>
-
     findByEmail(email: string): Promise<User | null>
 }
